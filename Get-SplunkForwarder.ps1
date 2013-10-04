1daffe7e08a9762aaf50b6b841175d1139703e74
Andy Newell <andrewtnewell@gmail.com>
2013-10-04 11:02:42 -0700

    stick to v5 after 6 was released
    Stick w/ V5..   Add test block for reg changes.

--- Get-SplunkForwarder.ps1
+++ Get-SplunkForwarder.ps1
@@ -7,10 +7,17 @@
 # Optionally, just print the download link to the pipeline with argument "-link"
 #
 
+# Edit 10.4.2013 
+#  Splunk 6 has been released;  we want to stay on V5 for now.  
+#
+
 # TODO: "set-executionpolicy remotesigned" before proceeding,  then pop the setting back when done
 #
 # TODO: programatically add the Splunk URI to "trusted sites" list.   
 #			Currently shows a modal dialog but does not interfere with script fucntion
+#		Also add test condition before firing the change
+#		redirect output to /dev/nul
+
 
 param(
  [Parameter(Mandatory=$false)] [switch]$link = $false,	#  -link to enable
@@ -21,53 +28,68 @@ $DebugPreference = "continue"
 
 # Add Splunk.com to trusted sites zone
 
+$working_dir = pwd
 push-location
 
-set-location "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings"
+$reg_path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings"
 
-	push-location ZoneMap\ESCDomains
+If (-not(Test-Path -Path "$reg_path\ZoneMap\EscDomains\splunk.com")) {
 
-	new-item splunk.com
-		push-location splunk.com
-		new-itemproperty . -Name "*" -Value 2 -Type DWORD
-	pop-location
+	set-location $reg_path
 
-	new-item security_powershell_ise.exe
-		push-location security_powershell_ise.exe
-		new-itemproperty . -Name about -Value 2 -Type DWORD
-	pop-location
+	push-location ZoneMap\ESCDomains  # down two
 
-	new-item //cdn.optimizely.com/js/7177505.js
-		push-location//cdn.optimizely.com/js/7177505.js
-		new-itemproperty . -Name about -Value 2 -Type DWORD
-	pop-location
+	new-item splunk.com | out-null
+		push-location splunk.com # down one
+		new-itemproperty . -Name "*" -Value 2 -Type DWORD | out-null
+	pop-location # up one
 
-	new-item googleadservices.com
-		push-location googleadservices.com
-		new-itemproperty . -Name "*" -Value 2 -Type DWORD
-	pop-location
+	new-item security_powershell_ise.exe | out-null
+		push-location security_powershell_ise.exe # down one
+		new-itemproperty . -Name about -Value 2 -Type DWORD | out-null
+	pop-location # up one
 
-	new-item google-analytics.com
-		push-location google-analytics.com
-		new-itemproperty . -Name "*" -Value 2 -Type DWORD
-	pop-location
+	#new-item //cdn.optimizely.com/js/7177505.js | out-null
+	#	push-location //cdn.optimizely.com/js/7177505.js # down one
+	#	new-itemproperty . -Name about -Value 2 -Type DWORD | out-null
+	#pop-location # up one
 
-pop-location
-pop-location
-pop-location
+	new-item googleadservices.com | out-null
+		push-location googleadservices.com # down one
+		new-itemproperty . -Name "*" -Value 2 -Type DWORD | out-null
+	pop-location # up one
+
+	new-item google-analytics.com | out-null
+		push-location google-analytics.com #d own one
+		new-itemproperty . -Name "*" -Value 2 -Type DWORD | out-null
+	pop-location #up one
+
+	# Out of the Registry, and back to where we started:
+	set-location $working_dir
+
+	}
 
 #  Get the Splunk Download page itself,  we're gonna scrape it for the link:
 #
-$splunk = Invoke-WebRequest https://www.splunk.com/download/universalforwarder -DisableKeepAlive -MaximumRedirection 0
+#$splunk = Invoke-WebRequest https://www.splunk.com/download/universalforwarder -DisableKeepAlive -MaximumRedirection 0
+
+$splunk = Invoke-WebRequest https://www.splunk.com/page/previous_releases -DisableKeepAlive -MaximumRedirection 0
 
 # Look for a link to an MSI package eding with "x64-release.msi".
 # There will be two links, Full Splunk and teh Forwarder.
 # The forwarder will be the second item in the zero-indexed array:
 # assemble the full URI from that.
-$suffix = ($splunk.links.innerHTML | Select-String x64-release.msi)[1]
+#$suffix = ($splunk.links.innerHTML | Select-String x64-release.msi)[1]
+
+
+$suffix =  $splunk.Links.innerHTML | Select-String -Pattern "^splunkforwarder-5.0.\d-\d+-x64-release.msi" | Sort-Object -Descending | Select-Object -First 1
+
 
 # Pick out the actual version number:
-$version = ($splunk.links.innerHTML | Select-String x64-release.msi)[1].tostring().Split("-")[1]
+#$version = ($splunk.links.innerHTML | Select-String x64-release.msi)[1].tostring().Split("-")[1]
+
+$version = $suffix.ToString().Split("-")[1]
+
 
 # Splunk wants to wrap the requests in a click-tracker.. Screw that.
 # The prefix below is the actual download URI:
@@ -90,7 +112,7 @@ if ($link -eq $true) {
 # ~30 Mb takes a moment to download, be patient!
 
 # Install the file..  Silently if Elevated,  otherwise throw a prompt to eleveate, say Yes.
-if ($install -eq $true) {
+if ($install -eq $true) {p
 	if ($link -eq $true) {
 		Write-Output "Error: Cannot specify -link in combination with -install; nothing to do."
 	}else{
